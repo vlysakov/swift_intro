fb4e183edd55a10376032691aff976ccdf28394b
Slava V. Lysakov <slava@lysakov.ru>
2020-03-17 00:08:08 +0300

    Добавлено решение задач о треугольнике и процентах из занятия 1

--- L1_Lysakov/L1_Lysakov/main.swift
+++ L1_Lysakov/L1_Lysakov/main.swift
@@ -10,9 +10,9 @@ import Foundation
 
 //Решение квадратного уравнения
 print("Введите значение a")
-let a :Double = Double(readLine() ?? "0") ?? 0
+var a :Double = Double(readLine() ?? "0") ?? 0
 print("Введите значение b")
-let b :Double = Double(readLine() ?? "0") ?? 0
+var b :Double = Double(readLine() ?? "0") ?? 0
 print("Введите значение c")
 let c :Double = Double(readLine() ?? "0") ?? 0
 
@@ -23,6 +23,31 @@ if (d>=0) {
     let x2 = (-b-sqrt(d))/(2*a)
     d != 0 ? print (String(format: "x1 = %.3f \nx2 = %.3f", x1, x2)) : print (String(format: "x = %.3f", x1))
 } else {
-    print ("Уравнение не имеет корней")
+    print ("Уравнение не имеет корней\n")
 }
+
+//Решение задачи о треугольнике
+print("Введите значение длины катета a")
+a = Double(readLine() ?? "0") ?? 0
+print("Введите значение длины катета b")
+b = Double(readLine() ?? "0") ?? 0
+if (a == 0 || b == 0) {
+    print ("Значение длины катета не может быть равно 0")
+} else {
+    let g = sqrt(a*a+b*b)
+    print (String(format: "Длина гипотенузы = %.3f\n", g))
+    print(String(format: "Периметр треугольника = %.3f\n", g+a+b))
+    print(String(format: "Площадь треугольника = %.3f\n", (a*b)/2))
+}
+
+//Решение задачи о процентах
+print("Введите сумму")
+var sum = Double(readLine() ?? "0") ?? 0
+print("Введите процент (1..100)")
+let p :Double = Double(readLine() ?? "0") ?? 0
+for _ in 1...5 {
+    sum += (sum*p/100)
+}
+print (String(format: " Итоговая сумма = %.3f\n", sum))
+
 
